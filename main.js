5e78c85a2e62b4f12ac3012a9166e71edc4e79dd
Muhammadm2211 <100311046+Muhammadm2211@users.noreply.github.com>
2022-02-26 15:11:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+
+    $('').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/left.svg" alt=""</button>', 
+        nextArrow: '<button type="button" class="slick-next"><img src="images/right.svg" alt=""</button>',
+        vertical: true
+    });
+
+
+});
